df2e1c7f704a0d8fd15f4f1ef1394013de60404e
lisabazdyreva <lisabazdyreva@gmail.com>
2022-10-24 13:29:28 +0300

    Fixes

--- src/store/api-actions/api-actions-filters/api-actions-filters.ts
+++ src/store/api-actions/api-actions-filters/api-actions-filters.ts
@@ -33,8 +33,8 @@ export const fetchLowPriceAction = createAsyncThunk<{lowPrice: number | ''}, {va
 }>(
   'filterCameras/fetchLowPrice',
   async ({value}, {getState, extra: api}) => {
-    if (value === '') {
-      return {lowPrice: value};
+    if (value === '' || value === 0) {
+      return {lowPrice: ''};
     }
 
     const params = {
@@ -57,8 +57,8 @@ export const fetchHighPriceAction = createAsyncThunk<{highPrice: number | ''}, {
 }>(
   'filterCameras/fetchHighPrice',
   async ({value}, {getState, extra: api}) => {
-    if (value === '') {
-      return {highPrice: value};
+    if (value === '' || value === 0) {
+      return {highPrice: ''};
     }
 
     const params = {
